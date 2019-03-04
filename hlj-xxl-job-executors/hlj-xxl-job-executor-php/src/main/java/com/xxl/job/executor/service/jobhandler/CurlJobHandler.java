a6d0bb5ba7cd67e258532e839262a2fd0c32961f
xia_xun <1529068093@qq.com>
2019-03-04 20:49:15 +0800

    curlHandler

--- hlj-xxl-job-executors/hlj-xxl-job-executor-php/src/main/java/com/xxl/job/executor/service/jobhandler/CurlJobHandler.java
+++ hlj-xxl-job-executors/hlj-xxl-job-executor-php/src/main/java/com/xxl/job/executor/service/jobhandler/CurlJobHandler.java
@@ -4,26 +4,40 @@ import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.IJobHandler;
 import com.xxl.job.core.handler.annotation.JobHandler;
 import com.xxl.job.core.log.XxlJobLogger;
+import com.xxl.job.executor.core.util.CommandParserUtil;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-/**
- * 命令行任务
- *
- * @author xuxueli 2018-09-16 03:48:34
- */
-@JobHandler(value="curlJobHandler")
+@JobHandler(value = "curlJobHandler")
 @Component
 public class CurlJobHandler extends IJobHandler {
 
+
     @Override
     public ReturnT<String> execute(String param) throws Exception {
-        String command = "curl " + param;
-        int exitValue = -1;
 
+        if (StringUtils.isEmpty(param)) {
+            return new ReturnT<String>(IJobHandler.FAIL.getCode(), "参数不能为空，请设置");
+        }
+
+        String command = "curl ";
+        String m = CommandParserUtil.getOptionValue("-m", param);
+        if (StringUtils.isEmpty(m)) {
+            command += "-m 10 ";
+        }
+
+        String connectTimeout = CommandParserUtil.getOptionValue("--connect-timeout", param);
+        if (StringUtils.isEmpty(connectTimeout)) {
+            command += "--connect-timeout 10 ";
+        }
+
+        command += param;
+
+        int exitValue = -1;
         BufferedReader bufferedReader = null;
         try {
             // command process
@@ -51,8 +65,9 @@ public class CurlJobHandler extends IJobHandler {
         if (exitValue == 0) {
             return IJobHandler.SUCCESS;
         } else {
-            return new ReturnT<String>(IJobHandler.FAIL.getCode(), "command exit value("+exitValue+") is failed");
+            return new ReturnT<String>(IJobHandler.FAIL.getCode(), "curl exit value(" + exitValue + ") is failed");
         }
     }
+
 
 }
