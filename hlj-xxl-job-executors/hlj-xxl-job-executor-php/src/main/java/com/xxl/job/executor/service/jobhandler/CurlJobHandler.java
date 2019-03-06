c14efd32b2ba8e80c5f3cadcdb5e6338124fb018
xia_xun <1529068093@qq.com>
2019-03-06 11:12:09 +0800

    commandHandler

--- hlj-xxl-job-executors/hlj-xxl-job-executor-php/src/main/java/com/xxl/job/executor/service/jobhandler/CurlJobHandler.java
+++ hlj-xxl-job-executors/hlj-xxl-job-executor-php/src/main/java/com/xxl/job/executor/service/jobhandler/CurlJobHandler.java
@@ -3,14 +3,9 @@ package com.xxl.job.executor.service.jobhandler;
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.IJobHandler;
 import com.xxl.job.core.handler.annotation.JobHandler;
-import com.xxl.job.core.log.XxlJobLogger;
-import com.xxl.job.executor.core.util.CommandParserUtil;
+import com.xxl.job.executor.core.util.CommandUtil;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 
 @JobHandler(value = "curlJobHandler")
 @Component
@@ -25,55 +20,18 @@ public class CurlJobHandler extends IJobHandler {
         }
 
         String command = "curl ";
-        String m = CommandParserUtil.getOptionValue("-m", param);
+        String m = CommandUtil.getOptionValue("-m", param);
         if (StringUtils.isEmpty(m)) {
             command += "-m 1200 ";
         }
 
-        String connectTimeout = CommandParserUtil.getOptionValue("--connect-timeout", param);
+        String connectTimeout = CommandUtil.getOptionValue("--connect-timeout", param);
         if (StringUtils.isEmpty(connectTimeout)) {
             command += "--connect-timeout 1200 ";
         }
 
         command += param;
-
-        System.out.println(command);
-
-        int exitValue = -1;
-        BufferedReader bufferedReader = null;
-        try {
-            // command process
-            Process process = Runtime.getRuntime().exec(command);
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(process.getInputStream());
-            bufferedReader = new BufferedReader(new InputStreamReader(bufferedInputStream));
-
-            StringBuffer sb = new StringBuffer();
-            // command log
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                XxlJobLogger.log(line);
-                sb.append(line);
-            }
-
-            String result = sb.toString();
-            System.out.println(result);
-
-            // command exit
-            process.waitFor();
-            exitValue = process.exitValue();
-        } catch (Exception e) {
-            XxlJobLogger.log(e);
-        } finally {
-            if (bufferedReader != null) {
-                bufferedReader.close();
-            }
-        }
-
-        if (exitValue == 0) {
-            return IJobHandler.SUCCESS;
-        } else {
-            return new ReturnT<String>(IJobHandler.FAIL.getCode(), "curl exit value(" + exitValue + ") is failed");
-        }
+        return CommandUtil.process(command);
     }
 
 
